b791f3aa484b02982c5c052aaa6fd07369273cd9
Andrew565 <andrew565@gmail.com>
2021-03-11 22:03:41 -0500

    WIP Adding grid layout constants

--- LAWSgrid.js
+++ LAWSgrid.js
@@ -1,38 +1,74 @@
+/**
+  * @typedef LAWSgridConfig
+  * @param {number} numRows
+  * @param {number} numCols
+  * @param {string} grid
+*/
+
+const gridConstants = {
+  e: "EMPTY",
+  EMPTY: "e",
+  
+  s: "SLOT",
+  SLOT: "s",
+  
+  f: "FOUNDATION",
+  FOUNDATION: "f",
+  
+  r: "ROW",
+  ROW: "r",
+  
+  d: "DRAW PILE",
+  DRAW_PILE: "d",
+  
+  w: "WASTE PILE",
+  WASTE_PILE: "w",
+  
+  o: "ALLOW ONE",
+  ALLOW_ONE: "o",
+  
+  n: "NO LIMIT",
+  NO_LIMIT: "n",
+  
+  h: "HORIZONTAL",
+  HORIZONTAL: "h",
+  
+  t: "TILT LEFT",
+  TILT_LEFT: "t",
+  
+  i: "TILT RIGHT",
+  TILT_RIGHT: "i",
+  
+  q: "FACE DOWN",
+  FACE_DOWN: "q",
+  
+  p: "FACE UP",
+  FACE_UP: "p"
+}
+
+const acesUpLayout = "rd4sw"
+const clearTheDungeonLayout = "r4sqr4sqr4sprd4sow"
+
 class LAWSgrid {
-  constructor(config = {}) {
-    config = {
-      numRows: 1,
-      numCols: 1,
-      modifiers: [["normal"]]
-    }
-  
-    grid = []
+  constructor(config) {
+    this.numRows = config.numRows || 1
+    this.numCols = config.numCols || 1
+    this.gridString = config.grid
+    this.grid = [[]]
     
-    this.config = {
-      ...this.config, ...config
+    this.createGrid()
+  }
+  
+  createGrid() {
+    const rows = this.gridString.split(gridConstants.ROW)
+    const row 
+  }
+  
+  static layoutGrid() {
+    this.grid.forEach(row => {
+      row.forEach(col => {
+        // apply mods
+      }
     }
-  }
-  
-  numberOfRows() {
-    return this.grid.length
-  }
-  
-  addRow(contents = []) {
-    this.config.numRows += 1
-    this.grid.push([contents])
-  }
-  
-  removeRow(rowIndex = -1) {
-    if (rowNum < 0 || this.numberOfRows > rowIndex) return void
-    this.grid.splice(rowIndex, 1)
-  }
-  
-  numberOfColumns(rowNumber) {
-    const row = this.grid[rowNumber]
-    return (!row) ? null : row.length
-  }
-  
-  addColumn(rowIndex, startIndex, contents) {
-    this.grid
   }
 }
